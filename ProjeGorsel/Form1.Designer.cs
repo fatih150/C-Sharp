67024c67ebc0b4c9c3d5c152851f4b8f0c22587e
fatih150 <fatihto.312@hotmail.com>
2023-04-13 18:09:04 +0300

    C-Sharp
    A math test with countdown and point
    system (5 questions, more questions
    on request)

---
+++ ProjeGorsel/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace ProjeGorsel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblZaman = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblGecenZaman = new System.Windows.Forms.Label();
+            this.lblNet = new System.Windows.Forms.Label();
+            this.lblYanlis = new System.Windows.Forms.Label();
+            this.lblDogru = new System.Windows.Forms.Label();
+            this.btnSonrSoru = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblZaman);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 208);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblZaman
+            // 
+            this.lblZaman.AutoSize = true;
+            this.lblZaman.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblZaman.Location = new System.Drawing.Point(577, 76);
+            this.lblZaman.Name = "lblZaman";
+            this.lblZaman.Size = new System.Drawing.Size(109, 39);
+            this.lblZaman.TabIndex = 6;
+            this.lblZaman.Text = "label3";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(13, 158);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(262, 38);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button1";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.DortButon);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(13, 123);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(262, 38);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.DortButon);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(13, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(262, 38);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.DortButon);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(13, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(262, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.DortButon);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(77, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lblGecenZaman);
+            this.panel2.Controls.Add(this.lblNet);
+            this.panel2.Controls.Add(this.lblYanlis);
+            this.panel2.Controls.Add(this.lblDogru);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(550, 208);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(250, 242);
+            this.panel2.TabIndex = 1;
+            this.panel2.Visible = false;
+            // 
+            // lblGecenZaman
+            // 
+            this.lblGecenZaman.AutoSize = true;
+            this.lblGecenZaman.Location = new System.Drawing.Point(31, 169);
+            this.lblGecenZaman.Name = "lblGecenZaman";
+            this.lblGecenZaman.Size = new System.Drawing.Size(98, 17);
+            this.lblGecenZaman.TabIndex = 3;
+            this.lblGecenZaman.Text = "Geçen Zaman";
+            // 
+            // lblNet
+            // 
+            this.lblNet.AutoSize = true;
+            this.lblNet.Location = new System.Drawing.Point(47, 129);
+            this.lblNet.Name = "lblNet";
+            this.lblNet.Size = new System.Drawing.Size(30, 17);
+            this.lblNet.TabIndex = 2;
+            this.lblNet.Text = "Net";
+            // 
+            // lblYanlis
+            // 
+            this.lblYanlis.AutoSize = true;
+            this.lblYanlis.Location = new System.Drawing.Point(31, 84);
+            this.lblYanlis.Name = "lblYanlis";
+            this.lblYanlis.Size = new System.Drawing.Size(46, 17);
+            this.lblYanlis.TabIndex = 1;
+            this.lblYanlis.Text = "Yanlış";
+            // 
+            // lblDogru
+            // 
+            this.lblDogru.AutoSize = true;
+            this.lblDogru.Location = new System.Drawing.Point(31, 36);
+            this.lblDogru.Name = "lblDogru";
+            this.lblDogru.Size = new System.Drawing.Size(47, 17);
+            this.lblDogru.TabIndex = 0;
+            this.lblDogru.Text = "Doğru";
+            // 
+            // btnSonrSoru
+            // 
+            this.btnSonrSoru.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSonrSoru.Location = new System.Drawing.Point(13, 260);
+            this.btnSonrSoru.Name = "btnSonrSoru";
+            this.btnSonrSoru.Size = new System.Drawing.Size(193, 70);
+            this.btnSonrSoru.TabIndex = 2;
+            this.btnSonrSoru.Text = "Başlat";
+            this.btnSonrSoru.UseVisualStyleBackColor = true;
+            this.btnSonrSoru.Click += new System.EventHandler(this.btnSonrSoru_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSonrSoru);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblNet;
+        private System.Windows.Forms.Label lblYanlis;
+        private System.Windows.Forms.Label lblDogru;
+        private System.Windows.Forms.Button btnSonrSoru;
+        private System.Windows.Forms.Label lblZaman;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblGecenZaman;
+    }
+}
+
---
+++ ProjeGorsel/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjeGorsel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int zaman = 0;
+        int sayac = 0;
+        double dogru = 0, yanlis = 0, net = 0;
+
+        void sorular()
+        {
+            if (sayac==1)
+            {
+                label2.Text = "Başkatsayısı 4, derecesi 3, sabit terimi 8, katsayılar toplamı 6 olan polinom aşağıdakilerden hangisidir?";
+                button1.Text = "4x²-6x+8";
+                button2.Text = "4x³+x²-7x+8";
+                button3.Text = "x³-4x²+8x-6";
+                button4.Text = "8x²+6x-4";
+                dogrucevap = button2.Text;
+            }
+            if (sayac == 2)
+            {
+                label2.Text = "2x²+ax+4=0,  x²-x-a=0  denklemlerinin birer kökü ortak ise a kaçtır?";
+                button1.Text = "4";
+                button2.Text = "5";
+                button3.Text = "6";
+                button4.Text = "7";
+                dogrucevap = button3.Text;
+
+            }
+
+            if (sayac == 3)
+            {
+                label2.Text = "f(x)=x²-4x+3 parabolünün eksenleri kestiği noktaları ve tepe noktasını köşe kabul eden dörtgenin alanı kaç br² dir?";
+                button1.Text = "4";
+                button2.Text = "5";
+                button3.Text = "8";
+                button4.Text = "10";
+                dogrucevap = button1.Text;
+
+            }
+
+            if (sayac == 4)
+            {
+                label2.Text = "1/sin²15+1/cos²15 işleminin sonucu hangisidir?";
+                button1.Text = "8";
+                button2.Text = "12";
+                button3.Text = "16";
+                button4.Text = "20";
+                dogrucevap = button3.Text;
+
+            }
+
+            if (sayac == 5)
+            {
+                label2.Text = "log50=a  ise  log0,016 aşağıdakilerden hangisine eşittir?";
+                button1.Text = "2-4a";
+                button2.Text = "4-3a";
+                button3.Text = "3a-5";
+                button4.Text = "5-4a";
+                dogrucevap = button4.Text;
+
+            }
+
+        
+    }
+
+        private void DortButon(object sender, EventArgs e)
+        {
+            foreach (Control item in panel1.Controls)
+            {
+                if (item is Button)
+                {
+                    item.Enabled = false;
+                }
+            }
+            if (sayac == 5)
+            {
+                panel1.Visible = false;
+                panel2.Visible = true;
+
+                btnSonrSoru.Visible = false;
+                MessageBox.Show("Matematik yarışması sona erdi...");
+            }
+
+            if ((sender as Button).Text == dogrucevap)
+            {
+                dogru++;
+            }
+            else
+            {
+                yanlis++;
+            }
+
+            net = dogru - (yanlis / 3);
+            lblDogru.Text = "Doğru Sayısı=" + dogru;
+            lblYanlis.Text = "Yanlış Sayısı=" + yanlis;
+            lblNet.Text = "Net Sayısı=" + net.ToString("#.##");
+            lblGecenZaman.Text = "Geçen Zaman" + zaman;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel2.Visible = false;
+            timer1.Interval = 1000;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            zaman++;
+            lblZaman.Text = zaman.ToString();
+        }
+
+        string dogrucevap = "";
+        private void btnSonrSoru_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            btnSonrSoru.Text = "Sonraki Soru";
+            sayac++;
+            label1.Text = "Soru " + sayac + ".";
+
+            foreach (Control item in panel1.Controls)
+            {
+                if (item is Button)
+                {
+                    item.Enabled = true;
+                }
+            }
+            sorular();
+        }
+    }
+}
